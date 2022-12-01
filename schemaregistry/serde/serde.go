12de31d72b879316e939b7d2a6a6f622570e62d7
Subhra Banerjee <b.subhraprakash@gmail.com>
2022-12-01 09:40:55 -0800

    Update serde.go

--- schemaregistry/serde/serde.go
+++ schemaregistry/serde/serde.go
@@ -150,12 +150,9 @@ func (s *BaseSerializer) GetID(topic string, msg interface{}, info schemaregistr
 		if err != nil {
 			return -1, err
 		}
-		id, err = s.Client.GetID(subject, info, false)
+		_, err := s.Client.GetID(subject, info, false)
 		if err != nil {
 			return -1, err
-		}
-		if id != useSchemaID {
-			return -1, fmt.Errorf("failed to match schema ID (%d != %d)", id, useSchemaID)
 		}
 	} else if useLatest {
 		metadata, err := s.Client.GetLatestSchemaMetadata(subject)
